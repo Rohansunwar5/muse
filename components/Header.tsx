f92580cb516c0f294191e7328db48e0f0d303220
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-27 04:08:32 +0530

    Update the home page with new components and styles.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children:  React.ReactNode;
@@ -78,8 +80,52 @@ const Header: React.FC<HeaderProps> = ({
           >
             <HiHome className="text-black" size={20} />
           </button>
+          <button
+            className="
+            rounded-full 
+            p-2 
+            bg-white 
+            flex 
+            items-center 
+            justify-center 
+            cursor-pointer 
+            hover:opacity-75 
+            transition
+          "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+          <div>
+            <Button
+              onClick={() => {}}
+              className="
+              bg-transparent
+            text-neutral-300
+              font-medium
+              "
+            >
+              Sign Up
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() => {}}
+              className="
+             bg-white 
+              px-6 
+              py-2
+              "
+            >
+              Log in 
+            </Button>
+          </div>
+          </>
         </div>
       </div>
+      {children} 
     </div>
   )
 }
